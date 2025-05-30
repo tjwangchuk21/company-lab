74a6fde8f35ee43561021638bb88f4e320ce9bba
tjwangchuk21 <thuksey.wangchuk@mcvts.org>
2025-05-30 08:50:41 -0400

    Create communications.cpp

---
+++ communications.cpp
@@ -0,0 +1,14 @@
+#include "communications.h"
+#include <algorithm>
+//Formats customer message responses.
+std::string greetCustomer(std::string& name) {
+    return "Hello " + name + ", and welcome to TJ's Golf!";
+}
+
+void formatAddress(std::string& address) {
+    std::transform(address.begin(), address.end(), address.begin(), ::toupper);
+}
+
+std::string closingMessage(std::string& productName) {
+    return "Thank you for choosing " + productName + ". We hope you enjoy it and come back to TJ's Golf!";
+}
