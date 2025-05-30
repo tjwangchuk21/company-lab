edb178baba018dd83c4ae6cb22fedc13589288fa
tjwangchuk21 <thuksey.wangchuk@mcvts.org>
2025-05-30 08:50:21 -0400

    Create communications.h

---
+++ communications.h
@@ -0,0 +1,10 @@
+#ifndef COMMUNICATIONS_H
+#define COMMUNICATIONS_H
+
+#include <string>
+//Declares customer communication functions
+std::string greetCustomer(std::string& name);
+void formatAddress(std::string& address);
+std::string closingMessage(std::string& productName);
+
+#endif
